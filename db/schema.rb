c9f447dcad32c59ef5ddf92a8e6bba04f0d33fd0
alwan mortada <alwanmortada@alwans-MacBook-Pro.local>
2018-03-01 22:40:56 -0600

    added relations ship between blogs and topics

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20161230234724) do
-
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 20180302042646) do
   enable_extension "plpgsql"
 
   create_table "blogs", id: :serial, force: :cascade do |t|
@@ -22,7 +8,9 @@ ActiveRecord::Schema.define(version: 20161230234724) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
@@ -54,4 +42,11 @@ ActiveRecord::Schema.define(version: 20161230234724) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
